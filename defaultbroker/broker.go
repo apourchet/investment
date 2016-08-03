96323289fec305325933f03e87cba323d35cdf6a
Antoine Pourchet <antoine@brkt.com>
2016-08-02 23:23:46 -0700

    Seems to work with the latest version of grpc

---
+++ defaultbroker/broker.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	pb "github.com/apourchet/investment/broker"
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+	"net"
+)
+
+type DefaultBroker struct {
+}
+
+func (b *DefaultBroker) GetQuote(ctx context.Context, qid *pb.QuoteID) (*pb.Quote, error) {
+	return nil, nil
+}
+
+func (b *DefaultBroker) StreamQuotes(qid *pb.QuoteID, stream pb.Broker_StreamQuotesServer) error {
+	return nil
+}
+
+func main() {
+	lis, _ := net.Listen("tcp", ":8080")
+
+	server := grpc.NewServer()
+	pb.RegisterBrokerServer(server, &DefaultBroker{})
+	server.Serve(lis)
+}
