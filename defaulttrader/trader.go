9438d72de341f79ce37883da5a5c0a6d0542157a
Antoine Pourchet <antoine@brkt.com>
2016-08-02 23:33:30 -0700

    Need to use what ive learned now :)

---
+++ defaulttrader/trader.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	pb "github.com/apourchet/investment/protos"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"io"
+)
+
+func main() {
+	conn, _ := grpc.Dial(":8080", grpc.WithInsecure())
+	defer conn.Close()
+
+	broker := pb.NewBrokerClient(conn)
+	qid := &pb.QuoteID{"EURUSD"}
+	stream, _ := broker.StreamQuotes(context.Background(), qid)
+
+	for {
+		q, err := stream.Recv()
+		if err == io.EOF {
+			// Done reading
+			return
+		}
+		fmt.Println(q.String())
+	}
+
+	// for {
+	// 	q, _ := broker.GetQuote(context.Background(), &pb.QuoteID{"EURUSD"})
+	// 	fmt.Println("Quote: " + q.String())
+	// 	time.Sleep(time.Second * 2)
+	// }
+}
---
+++ lib/broadcaster/broadcaster.go
@@ -0,0 +1,31 @@
+package broadcaster
+
+import (
+	"math/rand"
+)
+
+type Broadcaster struct {
+	receivers map[ReceiverID]chan interface{}
+}
+
+type ReceiverID int
+
+func NewBroadcaster() *Broadcaster {
+	return &Broadcaster{make(map[ReceiverID]chan interface{})}
+}
+
+func (b *Broadcaster) Register(cb chan interface{}) ReceiverID {
+	rid := ReceiverID(rand.Intn(10000))
+	b.receivers[rid] = cb
+	return rid
+}
+
+func (b *Broadcaster) Deregister(rid ReceiverID) {
+	delete(b.receivers, rid)
+}
+
+func (b *Broadcaster) Emit(data interface{}) {
+	for _, rec := range b.receivers {
+		go func(cb chan interface{}) { cb <- data }(rec)
+	}
+}
