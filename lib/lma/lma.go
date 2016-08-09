87e7bab041b7b116fce947166945d52c0b60b4a3
Antoine Pourchet <antoine.pourchet@gmail.com>
2016-08-08 23:33:46 -0700

    lma changed a bit

--- lib/lma/lma.go
+++ lib/lma/lma.go
@@ -3,36 +3,37 @@ package lma
 type Lma struct {
 	N      int
 	Steps  int
-	Values []float64
+	values []float64
+	Value  float64
 }
 
 func NewLma(n int) *Lma {
 	return &Lma{n, 0, make([]float64, n)}
 }
 
-func (l *Lma) Step(val float64) float64 {
+func (l *Lma) Step(val float64) {
 	if l.Steps == 0 {
 		for i := 0; i < l.N; i++ {
-			l.Values[i] = val
+			l.values[i] = val
 		}
-		return val
+		l.Value = val
 	}
 
 	l.rotate()
-	l.Values[0] = val
-	return l.compute()
+	l.values[0] = val
+	l.Value = l.compute()
 }
 
 func (l *Lma) rotate() {
 	for i := 1; i < l.N; i++ {
-		l.Values[i] = l.Values[i-1]
+		l.values[i] = l.values[i-1]
 	}
 }
 
 func (l *Lma) compute() float64 {
 	s := 0.
 	for i := 0; i < l.N; i++ {
-		s += l.Values[i] * float64(l.N-i)
+		s += l.values[i] * float64(l.N-i)
 	}
 	return s / float64(sum(l.N))
 }
